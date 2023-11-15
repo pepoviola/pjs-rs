9ec0b051637efb282ba9295f5b235ee477c62abc
Javier Viola <pepoviola@gmail.com>
2023-11-15 16:09:39 -0300

    cleans

--- testing/transfer.js
+++ testing/transfer.js
@@ -2,13 +2,11 @@
 const endpoint = arguments[0];
 const seed = arguments[1];
 
-console.log(0);
 const api = await pjs.api.ApiPromise.create({ provider: new pjs.api.WsProvider(endpoint) });
 await pjs.utilCrypto.cryptoWaitReady();
 const keyring = new pjs.keyring.Keyring({ type: "sr25519" });
 const signer = keyring.addFromUri(seed);
 
-console.log(1);
 // Make a transfer from Alice to Bob and listen to system events.
 // You need to be connected to a development chain for this example to work.
 const ALICE = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
@@ -21,10 +19,8 @@ const randomAmount = Math.floor((Math.random() * 100000) + 1);
 const transferAllowDeath = api.tx.balances.transferAllowDeath(BOB, randomAmount);
 
 return new Promise(async (resolve, _reject) => {
-    console.log(2);
     // Sign and Send the transaction
     const unsub = await transferAllowDeath.signAndSend(signer, ({ events = [], status }) => {
-        console.log(3);
         if (status.isInBlock) {
             console.log('Successful transfer of ' + randomAmount + ' with hash ' + status.asInBlock.toHex());
             return resolve();
