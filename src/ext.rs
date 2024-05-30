ecf2f92142b9ef62e12f7b6515ea487705d012ee
Javier Viola <pepoviola@gmail.com>
2024-05-30 17:16:26 +0200

    add basic ci in gha

--- src/ext.rs
+++ src/ext.rs
@@ -12,11 +12,7 @@ deno_core::extension!(
     pjs_extension,
     deps = [deno_url, deno_web, deno_websocket, deno_crypto],
     esm_entry_point = "ext:pjs_extension/src/runtime.js",
-    esm = [
-        "src/js/06_util.js",
-        "src/js/07.js",
-        "src/runtime.js",
-    ],
+    esm = ["src/js/06_util.js", "src/js/07.js", "src/runtime.js",],
     state = |state| {
         state.put(ZombiePermissions {});
     }
